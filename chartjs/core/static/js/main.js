6be3ba3da8a23b74b3ddd7aed7929123ed53e820
michaellan <michaellan.eng@gmail.com>
2019-11-26 19:41:21 -0500

    Validando funcionamiento JS

--- chartjs/core/static/js/main.js
+++ chartjs/core/static/js/main.js
@@ -1,8 +1,19 @@
 var ctx = document.getElementById('myChart').getContext('2d');
 var req = new XMLHttpRequest();
-let id = document.getElementById('get_data')
-const url = `http://127.0.0.1:8000/core/getdata/${parseInt(id)}'`;
-id.addEventListener()
+let id = document.getElementById('get_id')
+let btnBuscarId = document.getElementById('btn-get-data')
+
+btnBuscarId.addEventListener('click', () =>{
+
+    console.log('Hola, desde el boton')
+    if (id.value === null){
+        id = 1
+    }else{
+        id = id.value
+    }
+})
+console.log(id)
+// const url = `http://127.0.0.1:8000/core/getdata/${parseInt(id)}'`;
 
 req.open('GET', url)
 req.send()
@@ -12,56 +23,57 @@ req.onreadystatechange = function () {
         var datos = JSON.parse(req.response)
         console.log(datos)
         console.log(typeof(datos.data))
-
-
-        var chart = new Chart(ctx, {
-            // The type of chart we want to create
-            type: 'bar',
-        
-            // The data for our dataset
-            data: {
-                labels: datos.labels,
-                datasets: [{
-                    label: datos.name,
-                    backgroundColor: 'rgb(255, 99, 132)',
-                    borderColor: 'rgb(255, 99, 132)',
-                    data: datos.data,
-                }]
-            },
-        
-            // Configuration options go here
-            options: {
-                responsive: true,
-                title: {
-					display: true,
-					text: 'Chart.js Line Chart'
-				},
-				tooltips: {
-					mode: 'index',
-					intersect: false,
-				},
-                hover: {
-					mode: 'nearest',
-					intersect: true
-				},
-                scales: {
-					xAxes: [{
-						display: true,
-						scaleLabel: {
-							display: true,
-							labelString: 'Month'
-						}
-					}],
-					yAxes: [{
-						display: true,
-						scaleLabel: {
-							display: true,
-							labelString: 'Value'
-						}
-					}]
-				}
-            }
-        });
     }
 }
 
+
+function plot(datos){
+    var chart = new Chart(ctx, {
+        // The type of chart we want to create
+        type: 'bar',
+    
+        // The data for our dataset
+        data: {
+            labels: datos.labels,
+            datasets: [{
+                label: datos.name,
+                backgroundColor: 'rgb(255, 99, 132)',
+                borderColor: 'rgb(255, 99, 132)',
+                data: datos.data,
+            }]
+        },
+    
+        // Configuration options go here
+        options: {
+            responsive: true,
+            title: {
+                display: true,
+                text: 'Chart.js Line Chart'
+            },
+            tooltips: {
+                mode: 'index',
+                intersect: false,
+            },
+            hover: {
+                mode: 'nearest',
+                intersect: true
+            },
+            scales: {
+                xAxes: [{
+                    display: true,
+                    scaleLabel: {
+                        display: true,
+                        labelString: 'Month'
+                    }
+                }],
+                yAxes: [{
+                    display: true,
+                    scaleLabel: {
+                        display: true,
+                        labelString: 'Value'
+                    }
+                }]
+            }
+        }
+    });
+}
